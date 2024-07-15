0d8dc146c71c4c27e9e35aff1305609e3e6e0b1c
Paul Worthington <pworthington17@gmail.com>
2024-07-15 16:50:09 +0000

    initial

---
+++ test.py
@@ -0,0 +1,11 @@
+import csv
+
+data = [[10, 20, 30],[40, 50 , 60],[70, 80, 90]]
+
+file = open("test.csv", "w")
+
+writer = csv.writer(file)
+
+for row in data:
+    writer.writerow(row)
+file.close()
